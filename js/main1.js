b6100e41b260487d91f37c58a1db0322559f5ece
santosh sharma <sharmasantosh3161@gmail.com>
2019-12-06 00:36:29 +0530

    First commit

---
+++ js/main1.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/html.png') {
+    myImage.setAttribute ('src','images/html.png');
+  } else {
+    myImage.setAttribute ('src','images/html.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
